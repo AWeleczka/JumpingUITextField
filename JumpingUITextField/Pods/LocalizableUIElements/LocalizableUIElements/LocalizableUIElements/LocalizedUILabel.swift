48fb90edb6f03d2b621c6d9f62582630c73ac052
Alexander Weleczka <a.weleczka@schokopro.com>
2019-01-27 21:23:06 +0100

    added swiftlint and swiftformat buildsteps

--- JumpingUITextField/Pods/LocalizableUIElements/LocalizableUIElements/LocalizableUIElements/LocalizedUILabel.swift
+++ JumpingUITextField/Pods/LocalizableUIElements/LocalizableUIElements/LocalizableUIElements/LocalizedUILabel.swift
@@ -1,11 +1,11 @@
 //
-//  LocalizableUIElements - https://github.com/AWeleczka/LocalizableUIElements
-//  A set of UIKit-Elements with easy localization in code and storyboard
+//  JumpingUITextField - https://github.com/AWeleczka/JumpingUITextField
+//  Extension for UITextField written in swift 4.2
 //
-//  Readme:        https://github.com/AWeleczka/LocalizableUIElements/blob/master/README.md
-//  Changelog:     https://github.com/AWeleczka/LocalizableUIElements/blob/master/CHANGELOG.md
-//  CocoaPod:      https://cocoapods.org/pods/LocalizableUIElements
-//  License (MIT): https://github.com/AWeleczka/LocalizableUIElements/blob/master/LICENSE
+//  Readme:        https://github.com/AWeleczka/JumpingUITextField/blob/master/README.md
+//  Changelog:     https://github.com/AWeleczka/JumpingUITextField/blob/master/CHANGELOG.md
+//  CocoaPod:      https://cocoapods.org/pods/JumpingUITextField
+//  License (MIT): https://github.com/AWeleczka/JumpingUITextField/blob/master/LICENSE
 //
 //  Copyright (c) 2019 Alexander Weleczka - https://AWeleczka.de/
 //
